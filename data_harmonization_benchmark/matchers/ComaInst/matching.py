19b42bed17184a04eecd12133c2f43daca2191a9
EdenWuyifan <yfw215@nyu.edu>
2024-11-04 14:09:47 -0500

    feat: add Valentine matchers

---
+++ data_harmonization_benchmark/matchers/ComaInst/matching.py
@@ -0,0 +1,21 @@
+from typing import Any, Dict, Optional
+
+import pandas as pd
+from valentine import valentine_match
+from valentine.algorithms import Coma
+
+
+def matching(
+    usecase: str,
+    usecase_path: str,
+    source: pd.DataFrame,
+    target: pd.DataFrame,
+    top_k: int = 10,
+    use_gpu: bool = False,
+    config: Optional[Dict[str, Any]] = dict(),
+):
+    matcher = Coma(use_instances=True, java_xmx="10096m", max_n=top_k, **config)
+
+    matches = valentine_match(source, target, matcher)
+
+    return matches
---
+++ data_harmonization_benchmark/matchers/SimilarityFlooding/matching.py
@@ -0,0 +1,21 @@
+from typing import Any, Dict, Optional
+
+import pandas as pd
+from valentine import valentine_match
+from valentine.algorithms import SimilarityFlooding
+
+
+def matching(
+    usecase: str,
+    usecase_path: str,
+    source: pd.DataFrame,
+    target: pd.DataFrame,
+    top_k: int = 10,
+    use_gpu: bool = False,
+    config: Optional[Dict[str, Any]] = dict(),
+):
+    matcher = SimilarityFlooding(**config)
+
+    matches = valentine_match(source, target, matcher)
+
+    return matches
